2a130861f51db73cbbd5e1d3cc1aae55caa25c86
e531-devflops <e531.devflops@gmail.com>
2021-03-03 17:24:00 +0900

    initial commit

---
+++ src/main/java/kr/daoko/exam/SpringOauth2ExamApplication.java
@@ -0,0 +1,13 @@
+package kr.daoko.exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOauth2ExamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringOauth2ExamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/daoko/exam/SpringOauth2ExamApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.daoko.exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOauth2ExamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
